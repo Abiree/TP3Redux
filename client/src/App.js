e5971dffad197c5471086febf145945c72b8f6eb
Abiree <abireghallabii@gmail.com>
2021-04-16 17:22:52 +0000

    ShoppingList

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import AppNavbar from './components/AppNavBar'
+import ShoppingList from './components/ShoppingList';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div className="App">
       <AppNavbar/>
+      <ShoppingList/>
     </div>
   );
 }
